f54659cde98e24c79135dd8c4f2c791a405e4fc2
Italo Gabriel <108748417+Ytalul@users.noreply.github.com>
2024-05-12 11:42:24 -0300

    Update ThreadLetra.java

--- src/main/java/Threads/ThreadLetra.java
+++ src/main/java/Threads/ThreadLetra.java
@@ -5,6 +5,7 @@ public class ThreadLetra extends Thread {
 
     public ThreadLetra(ArrayList<Object> lista) {
         super( () -> {
+            System.out.println("ola");
             lista.removeIf(e -> e instanceof String);
         } );
     }
